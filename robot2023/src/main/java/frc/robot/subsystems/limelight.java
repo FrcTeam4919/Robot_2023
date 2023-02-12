3575b3ad3ef6768cbed37e7f0ab826d46dd2927c
MaryA075 <125149674+MaryA075@users.noreply.github.com>
2023-02-12 13:36:21 -0800

    new files added

---
+++ robot2023/src/main/java/frc/robot/subsystems/limelight.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+import frc.robot.commands.*;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance
+
+public class limelight extends SubsystemBase {
+    public limelight(){
+
+
+    }
+    
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run when in simulation
+
+    }
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+}
+
