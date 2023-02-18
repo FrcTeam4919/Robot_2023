932d7d7cd40ff77ae2e874accdc8729bc459936d
migeal <99619520+migeal@users.noreply.github.com>
2023-02-18 11:11:41 -0800

    finished commands

--- robot2023/src/main/java/frc/robot/commands/close.java
+++ robot2023/src/main/java/frc/robot/commands/close.java
@@ -50,6 +50,7 @@ public class close extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        m_claw.close();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
