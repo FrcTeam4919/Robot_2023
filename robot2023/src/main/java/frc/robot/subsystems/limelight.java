76086845ddcf63b88f84908034f83ab9d923ec62
RandalMcAllister <RainbowJester2@gmail.com>
2023-02-20 18:26:24 -0800

    robot2023/src/main/java/frc/robot/subsystems/limelight.java     awd awdasdsa.git/COMMIT_EDITMSG[+] [unix] (18:24 20/02/2023) 2,6 Top-- INSERT --das.git/COMMIT_EDITMSG[+] [unix] (18:24 20/02/2023) 2,6 Top-- INSERT --ada.git/COMMIT_EDITMSG[+] [unix] (18:24 20/02/2023) 2,6 Top-- INSERT --wdawda wd.git/COMMIT_EDITMSG[+] [unix] (18:24 20/02/2023) 2,6 Top-- INSERT --a.git/COMMIT_EDITMSG[+] [unix] (18:24 20/02/2023) 2,6 Top-- INSERT --

--- robot2023/src/main/java/frc/robot/subsystems/limelight.java
+++ robot2023/src/main/java/frc/robot/subsystems/limelight.java
@@ -6,14 +6,35 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
-//import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class limelight extends SubsystemBase {
     public limelight(){
-
-
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+        float Kp = -0.1f;
+        float min_command = 0.05f;
+        double heading_error = tx;
+        float steering_adjust = 0.0f;
     }
     
+    public void aimCone(){
+        double x = Limelight.getX()
+        
+        if (tx > 1.0)
+        {
+                steering_adjust = Kp*heading_error - min_command;
+        }
+        else if (tx < 1.0)
+        {
+                steering_adjust = Kp*heading_error + min_command;
+        }
+        left_command += steering_adjust;
+        right_command -= steering_adjust;
+    }
+
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
