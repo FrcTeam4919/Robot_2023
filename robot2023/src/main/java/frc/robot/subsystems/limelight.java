899b1ee3fec29ee148c9bfca1f49ff3b9df85f97
RandalMcAllister <RainbowJester2@gmail.com>
2023-02-26 16:50:00 -0800

    added range functionality

--- robot2023/src/main/java/frc/robot/subsystems/limelight.java
+++ robot2023/src/main/java/frc/robot/subsystems/limelight.java
@@ -50,6 +50,41 @@ public class limelight extends SubsystemBase {
         m_driveTrain.limelightDrive(left_command, right_command);
     }
 
+    public void aimRangeCone() {
+        
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+
+        double left_command = 0;
+        double right_command = 0;
+
+        float Kp = -0.1f;
+        float KpDistance = -0.1f;
+        float min_command = 0.05f;
+        double heading_error = tx.getDouble(0.0);
+        double distance_error = -ty.getDouble(0.0);
+        float steering_adjust = 0.0f;
+        
+        if (tx.getDouble(0.0) > 1.0)
+        {
+                steering_adjust = Kp * ((float)heading_error) - min_command;
+        }
+        else if (tx.getDouble(0.0) < 1.0)
+        {
+                steering_adjust = Kp * ((float)heading_error) + min_command;
+        }
+        
+        double distance_adjust = KpDistance * distance_error;
+        //Left and right are for tank drive, need to convert it to arcade or create tankdrive
+        // in the driveTrain subsystem (in addition to arcadedrive?)
+        left_command += steering_adjust + distance_adjust;
+        right_command -= steering_adjust + distance_adjust;
+
+        m_driveTrain.limelightDrive(left_command, right_command);
+    }
+
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
