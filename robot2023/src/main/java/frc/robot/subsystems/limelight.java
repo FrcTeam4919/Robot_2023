ce48f38ffe67985ae679448e7b2cc0d037792c6e
AstifoGuy69 <114313187+AstifoGuy69@users.noreply.github.com>
2023-03-05 13:42:23 -0800

    debugged and changed limelight

--- robot2023/src/main/java/frc/robot/subsystems/limelight.java
+++ robot2023/src/main/java/frc/robot/subsystems/limelight.java
@@ -6,13 +6,40 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 //import edu.wpi.first.networktables.NetworkTableInstance
 
 public class limelight extends SubsystemBase {
-    public limelight(){
+    private driveTrain m_driveTrain;
+   
+    public limelight(){}
+public void aimCone(){
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+NetworkTableEntry tx = table.getEntry("tx");
+NetworkTableEntry ty = table.getEntry("ty");
+NetworkTableEntry ta = table.getEntry("ta");
+float Kp = -0.1f;
+float min_command = 0.05f;
 
 
+
+        double heading_error = tx.getDouble(0.0) ;
+        float steering_adjust = 0.0f;
+        if(tx.getDouble(0.0)<1.0)
+       { 
+                if (heading_error < 0)
+                {
+
+                }
+                else
+                {
+
+                }
+        }
     }
+
+
+    
     
     @Override
     public void periodic() {
